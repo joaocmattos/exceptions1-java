c5cdae1f5ee5f15178a7f6e113162e35e1606bab
Joao <joaocunham11@gmail.com>
2026-03-26 10:54:43 -0300

    Good solution v2

--- src/appplication/Program.java
+++ src/appplication/Program.java
@@ -13,7 +13,7 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		try {
 			System.out.print("Room number: ");
 			int number = sc.nextInt();
@@ -21,27 +21,25 @@ public class Program {
 			Date checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			Date checkOut = sdf.parse(sc.next());
-	
+
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-	
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sc.next());			
-			
-			reservation.updateDates(checkIn, checkOut);		
+			checkOut = sdf.parse(sc.next());
+
+			reservation.updateDates(checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-		}
-		catch (ParseException e) {
+		} catch (ParseException e) {
 			System.out.println("Invalid date format");
-		}
-		catch (DomainException e) {
+		} catch (DomainException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
 		}
-		
+
 		sc.close();
 
 	}
--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -1,8 +1,8 @@
 package model.exceptions;
 
-public class DomainException extends Exception {
+public class DomainException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-	
+
 	public DomainException(String msg) {
 		super(msg);
 	}
